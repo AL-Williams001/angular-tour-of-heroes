61cb9d38e7431c35688169f5b05efe3402af223d
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-06-13 13:46:38 +0800

    add navigation to hero details component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { NgFor } from '@angular/common';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgFor,
+    HeroDetailComponent,
+    RouterModule,
+  ], // Add HeroDetailComponent here
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 
   getHeroes(): void {
